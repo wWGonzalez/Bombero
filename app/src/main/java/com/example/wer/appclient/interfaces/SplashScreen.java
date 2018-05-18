e127de0e6461c52b4e95bf5c18faeb065df04c8d
wWGonzalez <werner.e.2.g@gmail.com>
2018-05-18 14:11:34 -0600

    Splash Corregido para llamar Main

--- app/src/main/java/com/example/wer/appclient/interfaces/SplashScreen.java
+++ app/src/main/java/com/example/wer/appclient/interfaces/SplashScreen.java
@@ -3,14 +3,58 @@ package com.example.wer.appclient.interfaces;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
 
 public class SplashScreen extends AppCompatActivity {
+
+
+    String dato="";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, crearRegistro.class);
-        startActivity(intent);
-        finish();
+        leerFichero(); // buscar datos dentro del fichero
+        //tv1.setText(dato); //Muestra el usuario
+        if(dato.equals("")){
+
+            Intent intent = new Intent(this, crearRegistro.class);
+            startActivity(intent);
+            finish();
+        }
+        else{
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            finish();
+
+           // Toast.makeText(this,"Bienvendio "+dato, Toast.LENGTH_SHORT).show();
+            // Intent o = new Intent(this,PersonaFormulario.class);
+            // startActivity(o);
+        }
+       ;
+    }
+
+
+    private void leerFichero() {
+        try {
+            BufferedReader fin =
+                    new BufferedReader(
+                            new InputStreamReader(
+                                    openFileInput("Nombre.txt")));
+
+            String texto = fin.readLine();
+
+            // tv1.setText(texto);
+            this.dato = texto;
+            fin.close();
+        } catch (Exception ex) {
+            Log.e("Ficheros", "Error al leer fichero desde memoria interna");
+
+        }
     }
 }
